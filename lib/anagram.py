b5a4ab77bd301c1e7107828997997ad9265946b2
Primrose <primrose.njoki@student.moringaschool.com>
2025-05-16 22:43:03 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word=word
+    def match(self,possible_anagrams):
+        word_sorted=sorted(self.word.lower())
+        return[w for w in possible_anagrams if sorted (w.lower())==word_sorted
+               and w.lower()!=self.word]
+    
+listen=Anagram("listen")  
+listen.match(['enlists','google','inlets','banana'])
+print(matches)
